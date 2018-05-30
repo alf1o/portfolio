50cc03435a006488b55c0e9d786b76c5774ab22c
alfio parisi <alfioparisi.93@gmail.com>
2018-05-30 18:59:55 +0100

    goto icon is a link to the project GH page

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 import OpenInNew from '@material-ui/icons/OpenInNew';
 import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
@@ -21,6 +21,9 @@ const styles = theme => ({
     [theme.breakpoints.up('md')]: {
       width: '100%'
     }
+  },
+  avatar: {
+    backgroundColor: theme.palette.primary.main
   }
 });
 
@@ -37,14 +40,20 @@ function Project({ classes, project }) {
         avatar={
           <Avatar
             aria-label="project"
+            className={classes.avatar}
           >
             {project.icon}
           </Avatar>
         }
         action={
-          <IconButton>
+          <Button
+            color="primary"
+            href={project.url}
+            target="_blank"
+            rel="noopener"
+          >
             <OpenInNew />
-          </IconButton>
+          </Button>
         }
         title={project.name}
       >
