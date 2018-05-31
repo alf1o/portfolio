df3d77e13a76ffa50448521535cf786b6d9bfff9
alfio parisi <alfioparisi.93@gmail.com>
2018-05-31 14:28:14 +0100

    Home

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <Paper>
+      
+    </Paper>
+  );
+}
+
+export default Home;
