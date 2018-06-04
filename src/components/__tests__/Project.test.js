9b34df3d407017b0c10d3f83667beaba1c47e59d
alfio parisi <alfioparisi.93@gmail.com>
2018-06-04 23:10:18 +0100

    tests for ProjectTabs and Project

--- src/components/__tests__/Project.test.js
+++ src/components/__tests__/Project.test.js
@@ -1,6 +1,64 @@
 import React from 'react';
-import { Project } from '../Project';
+import Project from '../Project';
+import { createShallow } from '@material-ui/core/test-utils';
+import CardHeader from '@material-ui/core/CardHeader';
+import ListItem from '@material-ui/core/ListItem';
 
 describe('`Project`', () => {
-  it('should match snapshot');
+  let props = {};
+  let mountedPrj;
+  const shallow = createShallow();
+
+  function project() {
+    if (!mountedPrj) mountedPrj = shallow(<Project {...props} />);
+    return mountedPrj;
+  }
+
+  beforeEach(() => {
+    props = {
+      classes: {
+        card: '',
+        avatar: '',
+        description: '',
+        star: ''
+      },
+      project: {
+        icon: 'T',
+        url: 'test.com',
+        name: 'test',
+        description: 'test description',
+        features: ['to', 'test', 'or', 'not', 'to', 'test']
+      }
+    };
+    mountedPrj = undefined;
+  });
+
+  it('should render an `Avatar` icon which is `props.project.icon`', () => {
+    const avt = project().dive().find(CardHeader).props().avatar;
+    expect(avt.props.children).toBe(project().props().project.icon);
+  });
+
+  it('should render a `Button` which is a link to `project.url`', () => {
+    const btn = project().dive().find(CardHeader).props().action;
+    expect(btn.props.href).toBe(project().props().project.url);
+  });
+
+  it('should render a `CardHeader` with a `title` of `project.name`', () => {
+    const title = project().dive().find(CardHeader).props().title;
+    expect(title).toBe(project().props().project.name);
+  });
+
+  it('should render a `.description` with a text of `project.description`', () => {
+    const desc = project().dive().find('.Project-description-3');
+    expect(desc.props().children).toBe(project().props().project.description);
+  });
+
+  it('should render a `ListItem` for each `project.features`', () => {
+    const ls = project().dive().find(ListItem);
+    expect(ls.length).toBe(project().props().project.features.length);
+  });
+
+  it('should match snapshot', () => {
+    expect(project()).toMatchSnapshot();
+  });
 });
--- src/components/__tests__/ProjectTabs.test.js
+++ src/components/__tests__/ProjectTabs.test.js
@@ -1,11 +1,102 @@
 import React from 'react';
 import { ProjectTabs } from '../ProjectTabs';
+import { createShallow } from '@material-ui/core/test-utils';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 describe('`ProjectTabs`', () => {
-  // state.currentTab
-  // Tabs value should be currentTab
-  // changeTab
-  // Tabs onChange should be changeTab
-  // changeTab should update currentTab
-  it('should match snapshot');
+  let props = {};
+  let mountedTabs;
+  const shallow = createShallow();
+
+  function tabs() {
+    if (!mountedTabs) mountedTabs = shallow(<ProjectTabs {...props} />);
+    return mountedTabs;
+  }
+
+  beforeEach(() => {
+    props = {
+      classes: {
+        tabs: ''
+      },
+      match: {
+        match: {
+          url: 'test.com'
+        }
+      },
+      projects: [
+        {
+          name: 'hello'
+        },
+        {
+          name: 'world'
+        }
+      ]
+    };
+    mountedTabs = undefined;
+  });
+
+  it('should have a `state.currentTab` property', () => {
+    expect(tabs().state().currentTab).toBeDefined();
+  });
+
+  it('should have a `changeTab` method', () => {
+    expect(ProjectTabs.prototype.changeTab).toBeDefined();
+  });
+
+  describe('`changeTab`', () => {
+    const changeTabSpy = jest.spyOn(ProjectTabs.prototype, 'changeTab');
+
+    afterEach(() => {
+      changeTabSpy.mockClear();
+    });
+
+    it('should update `currentTab`', () => {
+      const ts = tabs().find(Tabs);
+      expect(tabs().state().currentTab).toBe(0);
+      ts.simulate('change', null, 3);
+      expect(tabs().state().currentTab).toBe(3);
+      ts.simulate('change', null, 1);
+      expect(tabs().state().currentTab).toBe(1);
+    });
+  });
+
+  describe('`Tabs`', () => {
+    it('should receive a `value` prop which is `changeTab`', () => {
+      const ts = tabs().find(Tabs);
+      expect(ts.props().value).toBe(tabs().state().currentTab);
+    });
+
+    it('should receive an `onChange` prop which is `changeTab`', () => {
+      const ts = tabs().find(Tabs);
+      expect(ts.props().onChange).toEqual(tabs().instance().changeTab);
+    });
+  });
+
+  it('should render as many `Tab`s as `props.projects`', () => {
+    const len = props.projects.length;
+    const ts = tabs().find(Tab);
+    expect(ts.length).toBe(len);
+  });
+
+  describe('`Tab`', () => {
+    it('should receive a `to` prop which is `url` + `project.name`', () => {
+      const ts = tabs().find(Tab);
+      const url = props.match.match.url;
+      ts.forEach((t, i) => {
+        expect(t.props().to).toBe(url + '/' + props.projects[i].name);
+      });
+    });
+
+    it('should receive a `label` prop `project.name`', () => {
+      const ts = tabs().find(Tab);
+      ts.forEach((t, i) => {
+        expect(t.props().label).toBe(props.projects[i].name);
+      });
+    });
+  });
+
+  it('should match snapshot', () => {
+    expect(tabs()).toMatchSnapshot();
+  });
 });
